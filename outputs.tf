564a4e6ca5f098ac869e3beafe6da6103aeda7a8
Dave Redmin <david.redmin@beta.dhs.gov>
2020-04-10 11:53:49 -0400

    Add all of the required and optional variables. Remove the unneeded variables from the skeleton. Add all of the new outputs and remove the unneeded outputs from the skeleton.

--- outputs.tf
+++ outputs.tf
@@ -1,24 +1,9 @@
-output "id" {
-  value       = aws_instance.example.id
-  description = "The EC2 instance ID"
+output "policy" {
+  value       = aws_iam_policy.s3_read
+  description = "The IAM policy that can read the specified objects from the specified S3 bucket."
 }
 
-output "arn" {
-  value       = aws_instance.example.arn
-  description = "The EC2 instance ARN"
-}
-
-output "availability_zone" {
-  value       = aws_instance.example.availability_zone
-  description = "The AZ where the EC2 instance is deployed"
-}
-
-output "private_ip" {
-  value       = aws_instance.example.private_ip
-  description = "The private IP of the EC2 instance"
-}
-
-output "subnet_id" {
-  value       = aws_instance.example.subnet_id
-  description = "The ID of the subnet where the EC2 instance is deployed"
+output "role" {
+  value       = aws_iam_role.s3_read
+  description = "The IAM role that can read the specified objects from the specified S3 bucket."
 }
--- variables.tf
+++ variables.tf
@@ -1,29 +1,57 @@
 # ------------------------------------------------------------------------------
-# REQUIRED PARAMETERS
+# Required parameters
 #
 # You must provide a value for each of these parameters.
 # ------------------------------------------------------------------------------
 
-variable "aws_region" {
-  description = "The AWS region to deploy into (e.g. us-east-1)"
+variable "entity_name" {
+  type        = string
+  description = "The name of the entity that the role is being created for (e.g. \"test-user\")."
 }
 
-variable "aws_availability_zone" {
-  description = "The AWS availability zone to deploy into (e.g. a, b, c, etc.)"
-}
-
-variable "subnet_id" {
-  description = "The ID of the AWS subnet to deploy into (e.g. subnet-0123456789abcdef0)"
+variable "s3_bucket" {
+  type        = string
+  description = "The name of the S3 bucket that the created role will be allowed to read from (e.g. \"my-bucket\")."
 }
 
 # ------------------------------------------------------------------------------
-# OPTIONAL PARAMETERS
+# Optional parameters
 #
 # These parameters have reasonable defaults.
 # ------------------------------------------------------------------------------
 
-variable "tags" {
-  type        = map(string)
-  description = "Tags to apply to all AWS resources created"
-  default     = {}
+variable "account_ids" {
+  type        = list(string)
+  description = "AWS account IDs that are allowed to assume the role."
+  default     = []
+}
+
+variable "aws_region" {
+  type        = string
+  description = "The AWS region where the non-global resources are to be provisioned (e.g. \"us-east-1\")."
+  default     = "us-east-1"
+}
+
+variable "iam_usernames" {
+  type        = list(string)
+  description = "The list of IAM usernames allowed to assume the role.  If not provided, defaults to allowing any user in the specified account(s).  Note that including \"root\" in this list will override any other usernames in the list."
+  default     = ["root"]
+}
+
+variable "role_description" {
+  type        = string
+  description = "The description to associate with the IAM role (as well as the corresponding policy) that allows read-only access to the specified object(s) in the specified S3 buckets.  Note that the first \"%s\" in this value will get replaced with the s3_bucket variable and the second \"%s\" will get replaced with the entity_name variable."
+  default     = "Allows read-only access to S3 bucket %s required for %s."
+}
+
+variable "role_name" {
+  type        = string
+  description = "The name to assign the IAM role (as well as the corresponding policy) that allows read-only access to the specified S3 buckets.  Note that the first \"%s\" in this value will get replaced with the s3_bucket variable and the second \"%s\" will get replaced with the entity_name variable."
+  default     = "%s-ReadOnly-%s"
+}
+
+variable "s3_objects" {
+  type        = list(string)
+  description = "A list specifying the objects in the S3 bucket that the created role will be allowed to read (e.g. [\"my-file\", \"projects\\example\\*\"]).  AWS-supported S3 ARN wildcards (* and ?) can be used, but full regular expressions can not.  If not specified, the role will be allowed to read any object in the bucket."
+  default     = ["*"]
 }
