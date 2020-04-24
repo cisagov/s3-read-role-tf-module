bc68cf57110b9dba045ca810d8190257cec7c720
Jeremy Frasier <jeremy.frasier@trio.dhs.gov>
2020-04-24 12:11:36 -0400

    Slightly tighten language in output descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "policy" {
   value       = aws_iam_policy.s3_read
-  description = "The IAM policy that can read the specified objects from the specified S3 bucket."
+  description = "The IAM policy that allows reading of the specified objects from the specified S3 bucket."
 }
 
 output "role" {
   value       = aws_iam_role.s3_read
-  description = "The IAM role that can read the specified objects from the specified S3 bucket."
+  description = "The IAM role that allows reading of the specified objects from the specified S3 bucket."
 }
