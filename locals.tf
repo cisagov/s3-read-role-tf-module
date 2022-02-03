60b531d56c6bc474d1013c152ed9725aa6566e03
David Redmin <david.redmin@trio.dhs.gov>
2022-02-03 17:05:41 -0500

    Truncate role name if over 64 characters after variable substitutions
    The current AWS limit for role names is 64 characters.

--- locals.tf
+++ locals.tf
@@ -9,6 +9,8 @@ locals {
 
   # If var.role_name contains two instances of "%s", use format()
   # to replace the first "%s" with var.s3_bucket and the second "%s"
-  # with var.entity_name, otherwise just use var.role_name as is
-  role_name = length(regexall(".*%s.*%s.*", var.role_name)) > 0 ? format(var.role_name, var.s3_bucket, var.entity_name) : var.role_name
+  # with var.entity_name, otherwise just use var.role_name as is.
+  # If the role name is longer than 64 characters (the current AWS limit),
+  # truncate it to the first 64 characters.
+  role_name = substr(length(regexall(".*%s.*%s.*", var.role_name)) > 0 ? format(var.role_name, var.s3_bucket, var.entity_name) : var.role_name, 0, 64)
 }
