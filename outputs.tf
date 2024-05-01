750ffa70e7500ef892cf9643f8e38dcdfa701e67
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2024-05-01 12:43:58 -0400

    Alphabetize

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "policy" {
-  value       = aws_iam_policy.s3_read
   description = "The IAM policy that can read the specified objects from the specified S3 bucket."
+  value       = aws_iam_policy.s3_read
 }
 
 output "role" {
-  value       = aws_iam_role.s3_read
   description = "The IAM role that can read the specified objects from the specified S3 bucket."
+  value       = aws_iam_role.s3_read
 }
